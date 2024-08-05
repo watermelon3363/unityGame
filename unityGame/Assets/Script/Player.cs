59906a3222495f0976b5eb59eae1ef632d160e78
watermelon3363 <lg28834703@gmail.com>
2024-08-05 12:39:56 +0900

    unity_Ontrigger_Object

--- unityGame/Assets/Script/Player.cs
+++ unityGame/Assets/Script/Player.cs
@@ -13,10 +13,14 @@ public class Player : MonoBehaviour
     public float a = 0f;
     public float b = 0f;
     public float c = 0f;
-    
+    public float timestart; // 시작
+    private float timeupdate;
+
     void Start()
     {
-        speed = 0.05f;
+        speed = 1f;
+        timestart = 0.0f;
+        timeupdate = 3.0f;
        
 
     }
@@ -25,6 +29,8 @@ public class Player : MonoBehaviour
     void Update()
     {
         PlayerMove();
+        Speed_Time();
+        
     }
 
     private void PlayerMove()
@@ -32,28 +38,28 @@ public class Player : MonoBehaviour
         
         if (Input.GetKey(KeyCode.D))
         {
-            a += speed * Time.deltaTime;
+            a -= speed * Time.deltaTime;
             transform.position = new Vector3(a, b, c);
 
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            a -= speed * Time.deltaTime;
+            a += speed * Time.deltaTime;
             transform.position = new Vector3(a, b, c);
 
         }
 
         if (Input.GetKey(KeyCode.W))
         {
-            c += speed * Time.deltaTime;
+            c -= speed * Time.deltaTime;
             transform.position = new Vector3(a, b, c);
 
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            c -= speed * Time.deltaTime;
+            c += speed * Time.deltaTime;
             transform.position = new Vector3(a, b, c);
 
         }
@@ -66,4 +72,68 @@ public class Player : MonoBehaviour
         //characterController.SimpleMove(movevecter * speed);
 
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("booster"))
+        {
+            speed += 5f;
+            Destroy(other.gameObject,3);
+            timestart = 0.0f;       
+
+
+            //while (timestart < 10) 
+            //{
+            //
+            //    timestart = timestart + timeupdate;
+            //
+            //    if (timestart > 10)
+            //    {
+            //        speed -= 10f;
+            //        break;
+            //    }
+            //}
+        if (other.CompareTag("slow"))
+            {
+                speed -= 5f;
+                Destroy(other.gameObject, 3);
+                timestart = 0.0f;  // slow오브젝트랑 충돌하면 timestart를 0.0f로 만들어줌. -> 플레이어가 slow오브젝트랑 충돌하고 난 후 timestart가 3.0f가 될 때까지 speed는 줄어든 상태,
+                                   // Speed_Time에 의해 timestart>timeupdate가 되면 다시 timestart를 0.0f로 만들어줌.  
+            }
+
+        if (other.CompareTag("door"))
+            {
+                Destroy(gameObject, 3);
+                timestart = 0.0f;
+                if (timestart == 1f)
+                {
+                    Debug.Log("1");
+                }
+                if (timestart == 2f)
+                {
+                    Debug.Log("2");
+                }
+                if (timestart == 3f)
+                {
+                    Debug.Log("3");
+                }
+            }
+        }
+    }
+    
+
+
+    private void Speed_Time() 
+    {
+        timestart += Time.deltaTime; //timestart는 0, timestar에 Time.deltaTime을 더해줌. == 시간이 흐름.
+
+        if (timestart > timeupdate) // timeupdate는 3.0, 3.0을 넘으면 player의 속도를 1f로 설정함. // timestart를 다시 0.0f로 만들어줌 
+        {
+            speed = 1f;
+            timestart = 0.0f;
+        }
+    }
+
 }
+
+    
